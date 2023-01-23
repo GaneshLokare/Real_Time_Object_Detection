4ab3a49f10ff256bf0fc748033a07bcc092f7748
Ganesh Lokare <lokaregns@gmail.com>
2023-01-23 14:22:40 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name="object_detection",
+    version="0.0.2",
+    author="Ganesh",
+    author_email="lokaregns@gmail.com",
+    packages= find_packages(),
+    install_requires=[],
+    )
